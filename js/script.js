e2fde26b90868d475fd5fad191b0d5a963163b58
VELHINHO\tiago <tiagovelhinho@gmail.com>
2022-12-12 20:40:20 +0000

    update 2

--- js/script.js
+++ js/script.js
@@ -112,6 +112,88 @@ window.onload = () =>{
     
 }
 
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement("div");
+  //Card should have category and should stay hidden 
+  card.classList.add("card", i.category, "hide");
+  //image div
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
+  //img tag
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+  //container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  //product name
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  //price
+  let price = document.createElement("h6");
+  price.innerText = i.price + "$" ;
+  container.appendChild(price);
+  //fuel
+  let fuel = document.createElement("h7");
+  fuel.classList.add("fuel");
+  fuel.innerText = i.fuel.toUpperCase();
+  container.appendChild(fuel);
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+
+function filterProduct(value) {
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  
+  let elements = document.querySelectorAll(".card");
+  elements.forEach((element) => {
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      if (element.classList.contains(value)) {
+        element.classList.remove("hide");
+      } else {
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+ 
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  elements.forEach((element, index) => {
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      cards[index].classList.remove("hide");
+    } else {
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+
+window.onload = () => {
+  filterProduct("all");
+};
+
 var swiper = new Swiper(".vehicles-slider", {
     slidesPerView: 1,
     spaceBetween: 20,
@@ -166,95 +248,4 @@ var swiper = new Swiper(".vehicles-slider", {
     },
   });
 
-  for (let i of products.data) {
-    //Create Card
-    let card = document.createElement("div");
-    //Card should have category and should stay hidden initially
-    card.classList.add("card", i.category, "hide");
-    //image div
-    let imgContainer = document.createElement("div");
-    imgContainer.classList.add("image-container");
-    //img tag
-    let image = document.createElement("img");
-    image.setAttribute("src", i.image);
-    imgContainer.appendChild(image);
-    card.appendChild(imgContainer);
-    //container
-    let container = document.createElement("div");
-    container.classList.add("container");
-    //product name
-    let name = document.createElement("h5");
-    name.classList.add("product-name");
-    name.innerText = i.productName.toUpperCase();
-    container.appendChild(name);
-    //price
-    let price = document.createElement("h6");
-    price.innerText = i.price + "$" ;
-    container.appendChild(price);
-    //fuel
-    let fuel = document.createElement("h7");
-    fuel.classList.add("fuel");
-    fuel.innerText = i.fuel.toUpperCase();
-    container.appendChild(fuel);
-  
-    card.appendChild(container);
-    document.getElementById("products").appendChild(card);
-  }
-  
-  //parameter passed from button (Parameter same as category)
-  function filterProduct(value) {
-    //Button class code
-    let buttons = document.querySelectorAll(".button-value");
-    buttons.forEach((button) => {
-      //check if value equals innerText
-      if (value.toUpperCase() == button.innerText.toUpperCase()) {
-        button.classList.add("active");
-      } else {
-        button.classList.remove("active");
-      }
-    });
-  
-    //select all cards
-    let elements = document.querySelectorAll(".card");
-    //loop through all cards
-    elements.forEach((element) => {
-      //display all cards on 'all' button click
-      if (value == "all") {
-        element.classList.remove("hide");
-      } else {
-        //Check if element contains category class
-        if (element.classList.contains(value)) {
-          //display element based on category
-          element.classList.remove("hide");
-        } else {
-          //hide other elements
-          element.classList.add("hide");
-        }
-      }
-    });
-  }
-  
-  //Search button click
-  document.getElementById("search").addEventListener("click", () => {
-    //initializations
-    let searchInput = document.getElementById("search-input").value;
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card");
-  
-    //loop through all elements
-    elements.forEach((element, index) => {
-      //check if text includes the search value
-      if (element.innerText.includes(searchInput.toUpperCase())) {
-        //display matching card
-        cards[index].classList.remove("hide");
-      } else {
-        //hide others
-        cards[index].classList.add("hide");
-      }
-    });
-  });
-  
-  //Initially display all products
-  window.onload = () => {
-    filterProduct("all");
-  };
+ 
